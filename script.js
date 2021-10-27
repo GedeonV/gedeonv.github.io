1624b570ba1fced6eb053e3fe505f2497f7d74cb
Arthur <zinni.arthur@gmail.com>
2021-10-27 18:32:09 +0200

    draCo

--- script.js
+++ script.js
@@ -110,13 +110,14 @@ function initialize(){
         type: 'pattern', patternUrl: "/data/nomad.patt",
     })
 
-    const dracoLoader = new THREE.DRACOLoader()
-    dracoLoader.setDecoderPath('/draco/')
 
     function onProgress(xhr) { console.log( (xhr.loaded / xhr.total * 100) + '% loaded' ); }
 	function onError(xhr) { console.log( 'An error happened' ); }
 
     const gltfLoader = new THREE.GLTFLoader()
+    const dracoLoader = new THREE.DRACOLoader()
+
+    dracoLoader.setDecoderPath('draco/')
     gltfLoader.setDRACOLoader(dracoLoader)
 
     gltfLoader.load(        
