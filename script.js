0f4c7d6c853affbd43f21cd20a71c68dc3ac56b7
Arthur <zinni.arthur@gmail.com>
2021-10-27 12:53:08 +0200

    test

--- script.js
+++ script.js
@@ -140,6 +140,7 @@ function initialize(){
 				.setPath( 'models/' )
 				.load( 'nomad.obj', function ( group ) {
                     group.children[0].material.side = THREE.DoubleSide;
+                    group.children[1].material.side = THREE.DoubleSide;
                     group.position.y = 0.25
                     group.scale.set(1.25, 1.25, 1.25)
 					markerRoot1.add(group);
