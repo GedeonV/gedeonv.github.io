16212b40876da0886a838446a6a0d8fcf0ed6cd7
Arthur <zinni.arthur@gmail.com>
2021-10-28 09:57:43 +0200

    retest

--- script.js
+++ script.js
@@ -1,73 +1,48 @@
-// import './style.css'
-// import * as THREE from 'three'
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-// import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-
-// import 'script-loader!./threex/ar-threex.js'
-
-/**
- * Base
- */
-
 var scene, camera, renderer, clock, deltaTime, totalTime;
 
 var arToolkitSource, arToolkitContext;
 
 var markerRoot1;
 
-document.addEventListener('DOMContentLoaded', function(evt) {
-    initialize();
-    animate();
-})
+var mesh1;
 
+initialize();
+animate();
 
+function initialize()
+{
+	scene = new THREE.Scene();
 
-function initialize(){
-    // Scene
-    scene = new THREE.Scene()
+	let ambientLight = new THREE.AmbientLight( 0xcccccc, 1.0 );
+	scene.add( ambientLight );
+				
+	camera = new THREE.Camera();
+	scene.add(camera);
 
-
-    /**
-     * Lights
-     */
-    const ambientLight = new THREE.AmbientLight(0xcccccc, 1.0)
-    scene.add(ambientLight)
-
-
-    /**
-     * Camera
-     */    
-
-    camera = new THREE.Camera();
-    scene.add(camera);
-
-    /**
-     * Renderer
-     */
-    renderer = new THREE.WebGLRenderer({antialias : true, alpha: true});
-	renderer.setSize(640, 480);
-    renderer.domElement.style.position = 'absolute'
+	renderer = new THREE.WebGLRenderer({
+		antialias : true,
+		alpha: true
+	});
+	renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+	renderer.setSize( 640, 480 );
+	renderer.domElement.style.position = 'absolute'
 	renderer.domElement.style.top = '0px'
 	renderer.domElement.style.left = '0px'
 	document.body.appendChild( renderer.domElement );
 
-    /**
-     * Animate
-     */
-    clock = new THREE.Clock()
-    deltaTime = 0
-    totalTime = 0
+	clock = new THREE.Clock();
+	deltaTime = 0;
+	totalTime = 0;
+	
+	////////////////////////////////////////////////////////////
+	// setup arToolkitSource
+	////////////////////////////////////////////////////////////
 
+	arToolkitSource = new THREEx.ArToolkitSource({
+		sourceType : 'webcam',
+	});
 
-    /**
-     * Setup de arToolkitSource
-     */
-
-    arToolkitSource = new THREEx.ArToolkitSource({
-        sourceType : 'webcam',
-    });
-
-    function onResize()
+	function onResize()
 	{
 		arToolkitSource.onResize()	
 		arToolkitSource.copySizeTo(renderer.domElement)	
@@ -81,34 +56,38 @@ function initialize(){
 		onResize()
 	});
 	
+	// handle resize event
 	window.addEventListener('resize', function(){
 		onResize()
 	});
+	
+	////////////////////////////////////////////////////////////
+	// setup arToolkitContext
+	////////////////////////////////////////////////////////////	
 
-    /**
-     * Setup de arToolkitContext
-     */
-
-    arToolkitContext = new THREEx.ArToolkitContext({
-        cameraParametersUrl: '/data/camera_para.dat',
-        detectionMode: 'mono',
-    });
-
-    arToolkitContext.init( function onCompleted(){
+	// create atToolkitContext
+	arToolkitContext = new THREEx.ArToolkitContext({
+		cameraParametersUrl: 'data/camera_para.dat',
+		detectionMode: 'mono'
+	});
+	
+	// copy projection matrix to camera when initialization complete
+	arToolkitContext.init( function onCompleted(){
 		camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
 	});
 
-    /**
-     * Setup du marker
-     */
+	////////////////////////////////////////////////////////////
+	// setup markerRoots
+	////////////////////////////////////////////////////////////
 
-    markerRoot1 = new THREE.Group();
-    scene.add(markerRoot1);
-    let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
-        type: 'pattern', patternUrl: "/data/nomad.patt",
-    })
-
-    const gltfLoader = new THREE.GLTFLoader()
+	// build markerControls
+	markerRoot1 = new THREE.Group();
+	scene.add(markerRoot1);
+	let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
+		type: 'pattern', patternUrl: "data/nomad.patt",
+	})
+	
+	const gltfLoader = new THREE.GLTFLoader()
     const dracoLoader = new THREE.DRACOLoader()
 
     dracoLoader.setDecoderPath('draco/')
@@ -125,15 +104,20 @@ function initialize(){
         )
 }
 
+
 function update()
 {
-    if ( arToolkitSource.ready !== false )
+	// update artoolkit on every frame
+	if ( arToolkitSource.ready !== false )
 		arToolkitContext.update( arToolkitSource.domElement );
 }
 
-function render(){
-    renderer.render( scene, camera );
+
+function render()
+{
+	renderer.render( scene, camera );
 }
+
 
 function animate()
 {
@@ -143,3 +127,6 @@ function animate()
 	update();
 	render();
 }
+
+
+
